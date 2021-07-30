7cef1f11284c57984e6454e01e12785f49884d1e
CassieTesauro <cassie.tesauro@gmail.com>
2021-07-30 10:58:50 -0500

    redo after git error- added event listener and export function in interiors.js

---
+++ scripts/interiors.js
@@ -0,0 +1,27 @@
+import { getInteriors } from "./database.js"; //import paint colors state from database
+
+const interiors = getInteriors() //put return of paint colors state exporting function in instantiated variable
+
+//make an event listener for when a user seelects a paint color from the paint color radio button html
+document.addEventListener(
+    "change",
+    (event) => {
+        if (event.target.name === "interior") {
+            window.alert(`You R choosing the interior ${event.target.value}`)
+        }
+    }
+)
+
+//Define the function that will put the list of paint color options into the html.  This gets imported on the main.js to be called in our html layout 
+export const Interiors = () => {
+    let html = "<ul>"
+
+    for (const interior of interiors) {
+        html += `<li>
+                    <input type="radio" name="interior" value="${interior.id}"/> ${interior.name}
+                 </li>`
+    }
+    
+    html += "</ul>"
+    return html
+}
